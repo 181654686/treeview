ec878a4a962bc2cafb074efac78d6b2ee4bf395a
tomliu0793 <181654686@qq.com>
2021-07-24 16:53:40 +0800

    Update key.dart

--- front_end/lib/key.dart
+++ front_end/lib/key.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -27,17 +28,26 @@ class _ShortcutTestState extends State<ShortcutTest> {
     super.initState();
   }
 
+  double _dx = 0.0;
+  double _dy = 0.0;
+  double _scale = 2.0;
+  String _key = '';
   @override
   Widget build(BuildContext context) {
+    double _previousScale;
+
     return RawKeyboardListener(
       focusNode: FocusNode()..requestFocus(),
       onKey: (event) {
-        print('Key Pressed: ${event.logicalKey}');
+        print('Key Pressed: ${event.logicalKey.keyLabel}');
         if (event.runtimeType == RawKeyDownEvent) {
           interactionManager.addKeyPressed(event.logicalKey);
+          _key = event.logicalKey.keyLabel;
         } else if (event.runtimeType == RawKeyUpEvent) {
           interactionManager.removeKeyPressed(event.logicalKey);
+          _key = '';
         }
+
         setState(() {});
         return null;
       },
@@ -48,7 +58,28 @@ class _ShortcutTestState extends State<ShortcutTest> {
               onPointerDown: (event) => {print('onPointerDown')},
               onPointerMove: (event) => {print('onPointerMove')},
               onPointerUp: (event) => {print('onPointerUp')},
-              onPointerSignal: (event) => {print('onPointerSignal')},
+              onPointerSignal: (event) {
+                if (event is PointerScrollEvent) {
+                  // print('x: ${event.position.dx}, y: ${event.position.dy}');
+                  // print('scroll delta: ${event.scrollDelta.dy}');
+
+                  setState(() {
+                    if (_key == '') {
+                      _dy = _dy + event.scrollDelta.dy / 5;
+                      print(_dy);
+                    }
+                    if (_key.toLowerCase() == 'control left') {
+                      if (event.scrollDelta.dy < 0) {
+                        _scale = _scale * 1.1;
+                        print(_scale);
+                      } else {
+                        _scale = _scale / 1.1;
+                        print(_scale);
+                      }
+                    }
+                  });
+                }
+              },
               onPointerHover: (event) => {print('onPointerHover')},
               child: Container(
                 width: double.infinity,
@@ -58,9 +89,16 @@ class _ShortcutTestState extends State<ShortcutTest> {
               ),
             ),
             Center(
-              child: Text(printKeysPressed(interactionManager.keyPressed),
-                  style: TextStyle(fontSize: 24, color: Colors.black)),
-            ),
+                child: Transform(
+                    transform: Matrix4.identity()
+                      ..translate(_dx, _dy)
+                      ..scale(_scale, _scale),
+                    child: Container(
+                      child: Text(
+                        'data',
+                        style: TextStyle(fontSize: 50),
+                      ),
+                    ))),
           ],
         ),
       ),
